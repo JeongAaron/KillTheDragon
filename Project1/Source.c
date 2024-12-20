6cf80fc7b7bd61e80c8765cc24a5701e7b86a238
JeongAaron <lovejsk1004@gmail.com>
2024-12-20 19:46:53 +0900

    2024-12-20 코딩게임 1일차

---
+++ Project1/Source.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <time.h>
+#include <Windows.h>
+#include <iostream>
+
+struct Charcter
+{
+	int PlayerHP;
+	int PlayerAP;
+	int PlayerDP;
+};
+
+struct Monster
+{
+	int PlayerHP;
+	int PlayerAP;
+	int PlayerDP;
+};
+
+void Battle(int x, int y)
+{
+	struct Charcter character;
+	if (x == 1 && y == 1)
+	{
+		if()
+	}
+}
+
+int main()
+{
+	
+
+	
+
+
+}
